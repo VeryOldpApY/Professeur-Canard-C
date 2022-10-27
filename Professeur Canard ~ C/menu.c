73b35f95bec22117cd1a54ae78ec5b2fa973e141
VeryOldpApY <dalix.playgamer@gmail.com>
2022-10-27 15:05:42 +0200

    Update Ex8

--- Professeur Canard ~ C/menu.c
+++ Professeur Canard ~ C/menu.c
@@ -4,15 +4,14 @@
 #include "exercice.h"
 #include "easter_egg.h"
 
-void Menu()
+void Menu()	
 {
 	int exec = 1;
 	do
 	{
 		int choiceTP;
 		system("cls");
-		printf("Quelle TP voulez vous executer ?\n(1-2) (0 : Return)\n");
-		printf("Easter Egg : (999 | 666)\n");
+		printf("Quelle TP voulez vous executer ?\n(1-2 : TP) (0 : Return)  (999 | 666 : Secret)\n");
 		scanf_s("%d", &choiceTP);
 
 		switch (choiceTP)
@@ -27,18 +26,23 @@ void Menu()
 				break;
 			case 666:
 				EasterEgg_2();
+				printf("\n\n\nAppuyez sur une touche pour continuer...");
+				getch();
 				break;
 			case 999:
 				EasterEgg_1();
+				printf("\n\n\nAppuyez sur une touche pour continuer...");
+				getch();
 				break;
 			default:
 				printf("Choix invalide.\n");
+				printf("\n\n\nAppuyez sur une touche pour continuer...");
+				getch();
 				break;
 		}
-		getch();
 		system("cls");
 		printf("Voulez vous continuez ?\nTrue (1), False (0)\n");
-		scanf_s("%d", &exec);		
+		scanf_s("%d", &exec);
 	} while (exec == 1);
 }
 
@@ -49,52 +53,53 @@ void MenuTP1()
 	{
 		int choiceEx;
 		system("cls");
-		printf("Quelle Exercice voulez vous executer ?\n(1-8) (0 : Return)\n");
+		printf("Quelle Exercice voulez vous executer ?\n(1-8 : Exercice) (0 : Return)\n");
 		scanf_s("%d", &choiceEx);
 		system("cls");
 
 		switch (choiceEx)
 		{
-			case 0:
-				break;
-			case 1:
-				TP1_Ex1();
-				break;
+		case 0:
+			break;
+		case 1:
+			TP1_Ex1();
+			break;
 
-			case 2:
-				TP1_Ex2();
-				break;
+		case 2:
+			TP1_Ex2();
+			break;
 
-			case 3:
-				TP1_Ex3();
-				break;
+		case 3:
+			TP1_Ex3();
+			break;
 
-			case 4:
-				TP1_Ex4();
-				break;
+		case 4:
+			TP1_Ex4();
+			break;
 
-			case 5:
-				TP1_Ex5();
-				break;
+		case 5:
+			TP1_Ex5();
+			break;
 
-			case 6:
-				TP1_Ex6();
-				break;
+		case 6:
+			TP1_Ex6();
+			break;
 
-			case 7:
-				TP1_Ex7();
-				break;
+		case 7:
+			TP1_Ex7();
+			break;
 
-			case 8:
-				TP1_Ex8();
-				break;
+		case 8:
+			TP1_Ex8();
+			break;
 
-			default:
-				printf("Choix invalide.\n");
-				break;
+		default:
+			printf("Choix invalide.\n");
+			break;
 		}
 
-		getch();
+		printf("\n\n\nAppuyez sur une touche pour continuer...");
+		getch()	;
 		system("cls");
 		printf("Voulez vous rester sur le TP 1?\nTrue (1), False (0)\n");
 		scanf_s("%d", &exec);
--- Professeur Canard ~ C/tableau.c
+++ Professeur Canard ~ C/tableau.c
@@ -3,38 +3,58 @@
 
 void Tableau()
 {
-	int nb = 0;
-	printf("Entrer la taille de votre tableau : (max 10)\n");
-	scanf_s("%d", &nb);
-	
-	int tab[10][10] = {0};
-	for (int i = 0; i < nb; i++)
+	int nb = -1;
+	while (nb != 0)
 	{
-		for (int j = 0; j < nb; j++)
+		printf("Entrer la taille de votre tableau : (max 10)\n");
+		scanf_s("%d", &nb);
+		if (nb == 0)
 		{
-			printf("Entrer le nombre : ");
-			scanf_s("%d", &tab[i][j]);
+			break;
 		}
-	}
 
-	for (int i = 0; i < nb; i++)
-	{
-		float moy = 0;
-		for (int j = 0; j < nb; j++)
+		int tab[10][10] = { 0 };
+		float totalV = 0;
+		float totalH = 0;
+		for (int i = 0; i < nb; i++)
 		{
-			moy += tab[i][j];
-			printf("%d	", tab[i][j]);
+			for (int j = 0; j < nb; j++)
+			{
+				printf("Entrer le nombre : ");
+				scanf_s("%d", &tab[i][j]);
+			}
 		}
-		printf("(%.2f)", moy / nb);
-		printf("\n");
-	}
-	for (int i = 0; i < nb; i++)
-	{
-		float moy = 0;
-		for (int j = 0; j < nb; j++)
+
+		for (int i = 0; i < nb; i++)
 		{
-			moy += tab[i][j];
+			float moy = 0;
+			for (int j = 0; j < nb; j++)
+			{
+				moy += tab[i][j];
+				printf("%d	", tab[i][j]);
+			}
+			printf("(%.2f)", moy / nb);
+			totalV += moy / nb;
+			printf("\n");
 		}
-		printf("(%.2f)  ", moy / nb);
+		for (int i = 0; i < nb; i++)
+		{
+			float moy = 0;
+			for (int j = 0; j < nb; j++)
+			{
+				moy += tab[j][i];
+			}
+			totalH += moy / nb;
+			printf("(%.2f)  ", moy / nb);
+		}
+		if (totalH == totalV)
+		{
+			printf("(%.2f)", totalH);
+		}
+		else
+		{
+			printf("(Error)");
+		}
+		printf("`\n");
 	}
 }
